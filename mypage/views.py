a72653bef3723f65403c8eb83c70f8b0af9917c1
andrey <nikitin.andrey201003@gmail.com>
2026-02-26 20:33:13 +0300

    Добавил секретный раздел со шпионским дизайном

--- mypage/views.py
+++ mypage/views.py
@@ -6,7 +6,22 @@ def home(request):
             <div style="text-align: center;">
                 <h1>Победа! Зеленый свет!</h1>
                 <p>Я кожаный мешок, и я умею в дизайн через Django.</p>
+                <br><br>
+<a href="/secret/" style="color: yellow; font-weight: bold;">[ ВОЙТИ В СЕКРЕТНЫЙ РАЗДЕЛ ]</a>
             </div>
         </body>
-    """) # И эти скобки — ПИШИ САМ
+    """)
+    
+
+def secret_rage(reuest):
+    return HttpResponse("""
+        <body style="background-color: black; color: #ff0000; font-family: 'Courier New', monospace; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+            <div style="text-align: center; border: 2px solid #ff0000; padding: 50px; box-shadow: 0 0 20px #ff0000;">
+                <h1>ДОСТУП РАЗРЕШЕН</h1>
+                <p>Добро пожаловать в секретный раздел, AndrewMasterLomaster.</p>
+                <p>Тут будут твои будущие мега-проекты.</p>
+                <a href="/" style="color: white; text-decoration: none; border-bottom: 1px dashed white;">Вернуться на главную</a>
+            </div>
+        </body>
+    """)
     
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from mypage.views import home
+from mypage.views import home, secret_rage
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home),
+    path("secret/", secret_rage),
 ]
