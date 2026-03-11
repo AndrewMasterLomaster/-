0ed70ce2b38f7cb5645741fcbaa767a11f441a9e
andrey <nikitin.andrey201003@gmail.com>
2026-03-11 21:01:15 +0300

    Добавил удаление и поиск по базе. Я машина

--- mypage/views.py
+++ mypage/views.py
@@ -1,5 +1,5 @@
 from .models import ContactMessage
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
     context = {
@@ -25,25 +25,40 @@ def secret_rage(request):
 
 
 def contacts(request):
-    status = None # Сначала статуса нет
+    status = None
     
+    # 1. Сначала ПРИЕМ (сохранение) нового сообщения
     if request.method == 'POST':
         name = request.POST.get('user_name')
         message = request.POST.get('user_message')
-        # Сохраняем в базу (один раз!)
         ContactMessage.objects.create(name=name, message=message)
         status = 'СООБЩЕНИЕ СОХРАНЕНО В БАЗУ!'
 
-    # ВАЖНО: Эти строки должны стоять БЕЗ лишних отступов (на уровне с "if")
-    # Чтобы они срабатывали ПРИ ЛЮБОМ заходе на страницу
-    all_messages = ContactMessage.objects.all().order_by("-created_at")
-    
+    # 2. Теперь ПОДГОТОВКА СПИСКА (всех сообщений)
+    # Сначала берем всё и сортируем (самые новые сверху)
+    all_messages = ContactMessage.objects.all().order_by('-created_at')
+
+    # 3. МАГИЯ ФИЛЬТРАЦИИ (Поиск)
+    query = request.GET.get('search') # Берем текст из поля поиска
+    if query:
+        # Оставляем только те, в которых имя СОДЕРЖИТ текст из поиска
+        all_messages = all_messages.filter(name__icontains=query)
+
+    # 4. ОТПРАВЛЯЕМ В ШАБЛОН
     context = {
         'all_msgs': all_messages,
         'status': status
     }
-    
     return render(request, 'contacts.html', context)
+
+
+def delete_message(request, msg_id):
+    # Находим сообщение по его номеру (ID)
+    msg = ContactMessage.objects.get(id=msg_id)
+      # Удаляем его из базы
+    msg.delete()
+       # Возвращаемся обратно на страницу контактов
+    return redirect("/contacts/")
 
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from mypage.views import home, secret_rage, contacts
+from mypage.views import home, secret_rage, contacts, delete_message
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home),
     path("secret/", secret_rage),
-    path("contacts/", contacts)
+    path("contacts/", contacts),
+    path("delete/<int:msg_id>/", delete_message, name="delete_msg"),
 ]
